a4e3a0f03aa082c655d9003c9a612a3b8e58991f
NUC <gaomeng@outlook.ie>
2023-08-17 23:52:40 +0800

    0817night

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,18 +4,19 @@ const app = getApp()
 Page({
   data: {
     navTitle: "",
-    tabs:["热门","正赛","野球"],
-    tabIcon:[
-      "http://ryt5dzeq0.hn-bkt.clouddn.com/dev/icon/hot-2x.png",
-      "http://ryt5dzeq0.hn-bkt.clouddn.com/dev/icon/official-2x.png",
-      "http://ryt5dzeq0.hn-bkt.clouddn.com/dev/icon/pink-ball-2x.png"
-      ],
+    tabs: ["热门", "正赛", "野球"],
+    tabIcon: [
+      "http://ryt5dzeq0.hn-bkt.clouddn.com/dev/icon/hot-3x.png",
+      "http://ryt5dzeq0.hn-bkt.clouddn.com/dev/icon/official-3x.png",
+      "http://ryt5dzeq0.hn-bkt.clouddn.com/dev/icon/pink-ball-3x.png"
+    ],
+    searchIcon: "http://ryt5dzeq0.hn-bkt.clouddn.com/dev/icon/search.png",
     active: '热门',
     showNarBar: false,
     loading: false,
     matchList: [],
     activeTab: 0,
-    swiperHeight:"100vh",
+    swiperHeight: "100vh",
     offsetTop: app.globalData.navBarHeight,
   },
   showNarBar(e) {
@@ -48,7 +49,7 @@ Page({
       })
     }
   },
-  onPageScroll(){
+  onPageScroll() {
     this.setSwiperHeight()
   },
   setSwiperHeight() {
@@ -68,7 +69,7 @@ Page({
             id: item.id,
             matchType: item.match_type,
             title: item.name,
-            date: formatForMatchCard(item.start_time) ,
+            date: formatForMatchCard(item.start_time),
             img: "https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg",
             address: item["location"],
             // avatars: ["头像地址1", "头像地址2"]
