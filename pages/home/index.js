3fb2006725c10eb8d7165f1c0b15ee50c2340dbd
NUC <gaomeng@outlook.ie>
2023-10-25 08:54:44 +0800

    部分重构

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,21 +1,17 @@
-import { getMatches } from '$/api'
-import { imgUrls } from '$/urls'
-import { iconSet } from '$/icon/index'
 
 const app = getApp()
-let old = false
-const classNameList = [".content-hot", ".content-official", ".content-diy"]
+let isFixed = false
+const classNames = [".hot", ".official", ".diy", ".arena"]
 
 Page({
   data: {
-    loading: true,
     navTitle: "老蓝孩俱乐部",
-    banner: imgUrls.bannerImg,
+    banner: 'https://openstore.obabyball.com/ui_v1/img/banner-img-v1.png',
     tabs: [
-      { title: "热门", icon: iconSet.tabHot },
-      { title: "正赛", icon: iconSet.tabOfficial },
-      { title: "野球", icon: iconSet.tabDiyGame },
-      { title: "场馆", icon: '' },
+      { title: "热门", icon: 'https://openstore.obabyball.com/ui_v1/icon/tab-hot-v1.svg' },
+      { title: "正赛", icon: 'https://openstore.obabyball.com/ui_v1/icon/tab-official-v1.svg' },
+      { title: "野球", icon: 'https://openstore.obabyball.com/ui_v1/icon/tab-diy-game-v2.svg' },
+      { title: "场馆", icon: 'https://openstore.obabyball.com/ui_v1/icon/tab-arena-v1.svg' },
     ],
     showNarBar: false,
     active: 0,
@@ -24,123 +20,52 @@ Page({
   },
 
   showNarBar(e) {
-    if (e.detail.isFixed === old) {
+    if (e.detail.isFixed === isFixed) {
       return
     } else {
       this.setData({
         showNarBar: e.detail.isFixed
       })
-      old = e.detail.isFixed
+      isFixed = e.detail.isFixed
     }
   },
 
   switchTab(e) {
-    console.log(e)
     this.setData({
       active: e.currentTarget.dataset.index
     })
   },
 
   onPullDownRefresh() {
-    wx.stopPullDownRefresh()
-    this.onLoad()
+
   },
 
   onShow() {
-
-    if (typeof this.getTabBar === 'function' &&
-      this.getTabBar()) {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
       this.getTabBar().setData({
         selected: 0
       })
     }
-    // if (app.globalData.currCity != "全国") {
-    //   let city = app.globalData.currCity
-    //   this.loadMatchList(city)
-    // }
   },
-  onPageScroll() {
+  onReady() {
+    this.setSwiperHeight()
   },
   setSwiperHeight() {
-    const className = classNameList[this.data.activeTab]
-    wx.createSelectorQuery().select(className).boundingClientRect(rect => {
-      if (rect.height >= app.globalData.windowHeight * 0.66) {
+    const currClassName = classNames[this.data.active]
+    wx.createSelectorQuery().select(currClassName).boundingClientRect(rect => {
+      if (rect.height >= app.globalData.common.windowHeight * 0.66) {
         this.setData({ swiperHeight: rect.height + 128 + 'px' });
       } else {
         this.setData({ swiperHeight: app.globalData.windowHeight * 0.66 + 'px' });
       }
     }).exec();
   },
-  onLoad() {
-    //订阅
-    // const { globalData } = getApp()
-    // globalData.setEvent('ON_CITY_CHANGE', this.loadMatchList);
 
-    // this.loadMatchList(globalData.currCity)
-  },
-  onUnload() {
-    const { globalData } = getApp();
-    globalData.removeEvent('ON_CITY_CHANGE', this.loadMatchList);
-  },
-  loadMatchList(city) {
-    city = city.replace("全国", "");
-    const filter = {
-      city: city,
-      match_type: 0,
-      page_size: 10,
-      page_token: "",
-      team_id: "",
-      user_id: ""
-    }
-    getMatches(filter).then(res => {
-      if (res.data.matches != null) {
-        const diyMatchList = res.data.matches.filter(item => item.match_type == 3)
-        this.setData({
-          diyMatchList,
-          matchList: res.data.matches
-        }, (() => {
-          this.setSwiperHeight()
-        }))
-      } else {
-        this.setData({
-          matchList: [],
-          diyMatchList: []
-        })
-      }
-    })
-  },
-  swiperChange(e) {
-    this.setData({
-      activeTab: e.detail.current
-    }, (() => {
-      this.setSwiperHeight()
-    }))
-  },
-  onReachBottom() {
-    // this.getMatchList()
-  },
-
-  onReady() {
-    this.setData({
-      loading: false,
-    });
-  },
   onShareAppMessage() {
     return {
       title: '老蓝孩',
       path: '/pages/home/index'
     }
   },
-  onShareTimeline() {
-    return {
-      title: '老蓝孩',
-    }
-  },
-  onClickFilterBtn(e) {
-    this.getTabBar().hide()
-    const key = e.currentTarget.dataset.key
-    this.setData({
-      [key]: true
-    })
-  }
+
 })
